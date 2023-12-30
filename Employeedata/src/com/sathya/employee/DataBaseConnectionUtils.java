d8c47ccd4ee425261f76f9c567ccaa88e79a1487
sathwikakistareddy <155213924+sathwikakistareddy@users.noreply.github.com>
2023-12-30 10:46:41 +0530

    Add files via upload

---
+++ Employeedata/src/com/sathya/employee/DataBaseConnectionUtils.java
@@ -0,0 +1,20 @@
+package com.sathya.employee;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConnectionUtils {
+    public static Connection createConnection()
+		{	
+			Connection connection = null; 	
+			try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","manager");	
+			}
+			catch(ClassNotFoundException | SQLException e)
+			{	e.printStackTrace();			
+			}
+			return connection;
+		}
+}
